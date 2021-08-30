e335b495201db80fb64033e7511aab8303268749
Dream023 <61343046+Dream023@users.noreply.github.com>
2021-08-30 10:16:59 +0700

    Frist

---
+++ Assets/Scrips/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody Player;
+    public float Speed = 20;
+    private int Score;
+    public Text score;
+    public GameObject YouWin;
+    void Start()
+    {
+        Player = GetComponent<Rigidbody>();
+        Score = 0;
+        YouWin.SetActive(false);
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        Player.AddForce(movement*Speed);
+        score.text = "Score : " + Score;
+        if(Score>=12)
+        {
+            YouWin.SetActive(true);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            other.gameObject.SetActive(false);
+            Score += 1;
+        }
+    }
+}
